f09589ece0248a27a60792d8c3f536f370c54772
feihoo87 <xfh1987@gmail.com>
2018-03-28 17:47:37 +0800

    add `plot` and `imshow`

--- lab/__init__.py
+++ lab/__init__.py
@@ -4,6 +4,6 @@ from ._bootstrap import (connect_db, get_current_notebook, get_current_user,
                          get_inputCells, listApps, listDrivers,
                          listInstruments, login, logout, open_resource,
                          save_inputCells, set_mode)
-from ._plot import make_figure_for_app, make_figures_for_App
+from ._plot import imshow, make_figure_for_app, make_figures_for_App, plot
 from ._query import query
 from ._version import __version__
