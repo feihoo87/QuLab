e0c682b957f0efaddb82a01b1c49d3f38f28f054
hzk2000 <37792502+hzk2000@users.noreply.github.com>
2018-04-09 16:51:21 +0800

    modify 81110A

--- drivers/81110A.py
+++ drivers/81110A.py
@@ -10,8 +10,15 @@ class Driver(BaseDriver):
     surport_models = ['81110A']
 
     quants = [
-        QReal('Frequency',unit='Hz',set_cmd='',get_cmd=''),
-        QReal('High Level',unit='V',set_cmd='',get_cmd=''),
-        QReal('Low Level',unit='V',set_cmd='',get_cmd=''),
-        QReal()
+
+        QReal('CH1 Delay',unit='ns',set_cmd=':PULS:DEL1 %(value).2fNS',get_cmd=':PULS:DEL1?'),
+        QReal('CH1 Width',unit='ns',set_cmd=':PULS:WIDT1 %(value).2fNS',get_cmd=':PULS:WIDT1?'),
+        QReal('CH1 High Level',unit='V',set_cmd=':VOLT1:HIGH %(value).2fV',get_cmd=':VOLT1:HIGH?'),
+        QReal('CH1 Low Level',unit='mV',set_cmd=':VOLT1:LOW %(value).2fMV',get_cmd=':VOLT1:LOW?'),
+        QReal('CH1 Offset',unit='mV',set_cmd=':VOLT1:OFF %(value).2fMV',get_cmd=':VOLT1:OFF?'),
+        QReal('Trigger Level',unit='V',set_cmd=':ARM:LEV %(value).2fV',get_cmd=':ARM:LEV?'),
+        QReal('Trigger Period',unit='ns',set_cmd=':ARM:PER %(value).2fns',get_cmd=':ARM:PER?'),
+
+
+        #QReal('T0 Amplitude', unit='V', set_cmd='LAMP 0,%(value).2f', get_cmd='LAMP?0'),
     ]
