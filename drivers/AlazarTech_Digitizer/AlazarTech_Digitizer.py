0c9911a9715590fdddceee919f7ff2fdda4d7cfb
feihoo87 <xfh1987@gmail.com>
2018-04-20 17:13:14 +0800

    fix AlazerTech driver

--- drivers/AlazarTech_Digitizer/AlazarTech_Digitizer.py
+++ drivers/AlazarTech_Digitizer/AlazarTech_Digitizer.py
@@ -211,7 +211,17 @@ class Driver(BaseDriver):
 
     def setHeterodyneFrequency(self, samplesPerRecord, heterodyne_freq=[]):
         Exp = []
-        t = np.arange(0, samplesPerRecord, 1) * 1e-9
+        sampleRate = {
+            '1k': 1e3, '2k': 2e3, '5k': 5e3, '10k': 10e3,
+            '20k': 20e3, '50k': 50e3, '100k': 100e3, '200k': 200e3,
+            '500k': 500e3, '1M': 1e6, '2M': 2e6, '5M': 5e6,
+            '10M': 10e6, '20M': 20e6, '25M': 25e6, '50M': 50e6,
+            '100M': 100e6, '125M': 125e6, '160M': 160e6, '180M': 180e6,
+            '200M': 200e6, '250M': 250e6, '400M': 400e6, '500M': 500e6,
+            '800M': 800e6, '1G': 1e9, '1200M': 1.2e9, '1500M': 1.5e9,
+            '1600M': 1.6e9, '1800M': 1.8e9, '2G': 2e9,
+        }[self.getValue('Sample Rate')]
+        t = np.arange(0, samplesPerRecord, 1) / sampleRate
         for f in heterodyne_freq:
             Exp.append(np.exp(1j*2*np.pi*f*t))
         self._Exp = np.asarray(Exp).T
