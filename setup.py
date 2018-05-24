8fce948a5c787fd59083188326869040134f0000
feihoo87 <xfh1987@gmail.com>
2018-05-24 21:36:32 +0800

    edit classifiers

--- setup.py
+++ setup.py
@@ -48,17 +48,15 @@ setup(
     },
     python_requires='>=3.6',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Pick your license as you wish (should match "license" above)
-         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
     ],
     project_urls={  # Optional
