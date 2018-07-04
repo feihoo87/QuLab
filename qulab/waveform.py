ef047b68be93c16ae39208b77d9db060b3ffa4fb
feihoo87 <xfh1987@gmail.com>
2018-07-04 17:16:49 +0800

    refactor(waveform): change calc to timeFunc

--- qulab/waveform.py
+++ qulab/waveform.py
@@ -25,57 +25,57 @@ class Waveform():
         outside: 定义域之外的默认值
         '''
         self._domain = domain
-        self._calc_domain = (-np.inf, np.inf)
+        self._timeFunc_domain = (-np.inf, np.inf)
         self._outside = outside
         self._time_shift = 0
-        self.calc = lambda x : 0
+        self.timeFunc = lambda x : 0
 
     def _mask(self, x):
-        mask = (x>self._calc_domain[0])*(x<self._calc_domain[1])
-        fmask = (x<=self._calc_domain[0])
-        bmask = (x>=self._calc_domain[1])
+        mask = (x>self._timeFunc_domain[0])*(x<self._timeFunc_domain[1])
+        fmask = (x<=self._timeFunc_domain[0])
+        bmask = (x>=self._timeFunc_domain[1])
         return fmask, mask, bmask
 
-    def __a_point_in_calc_domain(self):
-        if self._calc_domain[0] == -np.inf and self._calc_domain[1] == np.inf:
+    def __a_point_in_timeFunc_domain(self):
+        if self._timeFunc_domain[0] == -np.inf and self._timeFunc_domain[1] == np.inf:
             return 0
-        elif self._calc_domain[0] == -np.inf:
-            return self._calc_domain[1] - 1
-        elif self._calc_domain[1] == np.inf:
-            return self._calc_domain[0] + 1
+        elif self._timeFunc_domain[0] == -np.inf:
+            return self._timeFunc_domain[1] - 1
+        elif self._timeFunc_domain[1] == np.inf:
+            return self._timeFunc_domain[0] + 1
         else:
-            return 0.5*(self._calc_domain[0]+self._calc_domain[1])
+            return 0.5*(self._timeFunc_domain[0]+self._timeFunc_domain[1])
 
-    def _calc(self, x):
+    def _timeFunc(self, x):
         fmask, mask, bmask = self._mask(x-self._time_shift)
-        x = mask*(x-self._time_shift)+self.__a_point_in_calc_domain()*(fmask+bmask)
-        return fmask*self._outside[0] + bmask*self._outside[1] + mask*self.calc(x)
+        x = mask*(x-self._time_shift)+self.__a_point_in_timeFunc_domain()*(fmask+bmask)
+        return fmask*self._outside[0] + bmask*self._outside[1] + mask*self.timeFunc(x)
 
     def generateData(self, sampleRate, with_x=False):
         x = np.arange(self._domain[0], self._domain[1], 1.0/sampleRate)
         if with_x:
-            return x, self._calc(x)
+            return x, self._timeFunc(x)
         else:
-            return self._calc(x)
+            return self._timeFunc(x)
 
     def _comb_waveform(self, other):
         domain = _comb_domain(self._domain, other._domain)
         outside = _comb_outside(self._domain, other._domain, self._outside, other._outside)
         w = Waveform(domain=domain, outside=outside)
-        start = min(self._calc_domain[0], other._calc_domain[0])
-        stop = max(self._calc_domain[1], other._calc_domain[1])
-        w._calc_domain = (start, stop)
-        #w._calc_domain = self._comb_calc_domain(other)
+        start = min(self._timeFunc_domain[0], other._timeFunc_domain[0])
+        stop = max(self._timeFunc_domain[1], other._timeFunc_domain[1])
+        w._timeFunc_domain = (start, stop)
+        #w._timeFunc_domain = self._comb_timeFunc_domain(other)
         #w._time_shift = self._time_shift
         return w
 
     def __call__(self, x):
-        return self._calc(x)
+        return self._timeFunc(x)
 
     def __add__(self, other):
         if isinstance(other, Waveform):
             w = self._comb_waveform(other)
-            w.calc = lambda x : self._calc(x) + other._calc(x)
+            w.timeFunc = lambda x : self._timeFunc(x) + other._timeFunc(x)
             w._outside = (self._outside[0]+other._outside[0], self._outside[1]+other._outside[1])
             return w
         else:
@@ -84,14 +84,14 @@ class Waveform():
     def __radd__(self, v):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : v + self._calc(x)
+        w.timeFunc = lambda x : v + self._timeFunc(x)
         w._outside = (self._outside[0]+v, self._outside[1]+v)
         return w
 
     def __sub__(self, other):
         if isinstance(other, Waveform):
             w = self._comb_waveform(other)
-            w.calc = lambda x : self._calc(x) - other._calc(x)
+            w.timeFunc = lambda x : self._timeFunc(x) - other._timeFunc(x)
             w._outside = (self._outside[0]-other._outside[0], self._outside[1]-other._outside[1])
             return w
         else:
@@ -100,14 +100,14 @@ class Waveform():
     def __rsub__(self, v):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : v - self._calc(x)
+        w.timeFunc = lambda x : v - self._timeFunc(x)
         w._outside = (v-self._outside[0], v-self._outside[1])
         return w
 
     def __mul__(self, other):
         if isinstance(other, Waveform):
             w = self._comb_waveform(other)
-            w.calc = lambda x : self._calc(x) * other._calc(x)
+            w.timeFunc = lambda x : self._timeFunc(x) * other._timeFunc(x)
             w._outside = (self._outside[0]*other._outside[0], self._outside[1]*other._outside[1])
             return w
         else:
@@ -116,14 +116,14 @@ class Waveform():
     def __rmul__(self, v):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : v * self._calc(x)
+        w.timeFunc = lambda x : v * self._timeFunc(x)
         w._outside = (self._outside[0]*v, self._outside[1]*v)
         return w
 
     def __truediv__(self, other):
         if isinstance(other, Waveform):
             w = self._comb_waveform(other)
-            w.calc = lambda x : self._calc(x) / other._calc(x)
+            w.timeFunc = lambda x : self._timeFunc(x) / other._timeFunc(x)
             w._outside = (self._outside[0]*other._outside[0], self._outside[1]*other._outside[1])
             return w
         else:
@@ -132,14 +132,14 @@ class Waveform():
     def __rtruediv__(self, v):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : v / self._calc(x)
+        w.timeFunc = lambda x : v / self._timeFunc(x)
         w._outside = (v/self._outside[0], v/self._outside[1])
         return w
 
     def __pow__(self, v):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : self._calc(x)**v
+        w.timeFunc = lambda x : self._timeFunc(x)**v
         w._outside = (self._outside[0]**v, self._outside[1]**v)
         return w
 
@@ -149,14 +149,14 @@ class Waveform():
     def __neg__(self):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : - self._calc(x)
+        w.timeFunc = lambda x : - self._timeFunc(x)
         w._outside = (-self._outside[0], -self._outside[1])
         return w
 
     def __abs__(self):
         #w = copy.deepcopy(self)
         w = Waveform(domain=self._domain, outside=self._outside)
-        w.calc = lambda x : abs(self._calc(x))
+        w.timeFunc = lambda x : abs(self._timeFunc(x))
         w._outside = (abs(self._outside[0]), abs(self._outside[1]))
         return w
 
@@ -164,8 +164,8 @@ class Waveform():
         w = Waveform((self._domain[0], self._domain[1]+other.len()))
         w._time_shift = self._time_shift
         w._outside = (self._outside[0], other._outside[1])
-        #w.calc = lambda x: self._calc(x) if x < self._domain[1] else other._calc(x-self._domain[1]+other._domain[0])
-        w.calc = lambda x: self._calc(x) * (x < self._domain[1]) + other._calc(x-self._domain[1]+other._domain[0]) * (x >= self._domain[1])
+        #w.timeFunc = lambda x: self._timeFunc(x) if x < self._domain[1] else other._timeFunc(x-self._domain[1]+other._domain[0])
+        w.timeFunc = lambda x: self._timeFunc(x) * (x < self._domain[1]) + other._timeFunc(x-self._domain[1]+other._domain[0]) * (x >= self._domain[1])
         return w
 
     def __xor__(self, n):
@@ -182,7 +182,7 @@ class Waveform():
         w = copy.deepcopy(self)
         w._time_shift = self._time_shift + t
         w._domain = (self._domain[0]+t, self._domain[1]+t)
-        w._calc_domain = (self._calc_domain[0], self._calc_domain[1])
+        w._timeFunc_domain = (self._timeFunc_domain[0], self._timeFunc_domain[1])
         return w
 
     def __lshift__(self, t):
@@ -193,8 +193,8 @@ class Waveform():
 
     def overwrite(self, other):
         w = self._comb_waveform(other)
-        w.calc = lambda x : other._calc(x) * (x >= other._domain[0])*( x <= other._domain[1]) \
-                          + self._calc(x) * ((x >= other._domain[0])+( x <= other._domain[1]))
+        w.timeFunc = lambda x : other._timeFunc(x) * (x >= other._domain[0])*( x <= other._domain[1]) \
+                          + self._timeFunc(x) * ((x >= other._domain[0])+( x <= other._domain[1]))
         return w
 
     def set_range(self, t1, t2):
@@ -216,9 +216,9 @@ class DC(Waveform):
             self.stop = length
         super(DC, self).__init__(domain=(self.start, self.stop))
         self._DC = offset
-        self.calc = lambda x: self._DC * (x > self.start) * (x < self.stop)
+        self.timeFunc = lambda x: self._DC * (x > self.start) * (x < self.stop)
 
-    #def _calc(self, x):
+    #def _timeFunc(self, x):
     #    x = x - self._time_shift
     #    return self._DC * (x > self.start) * (x < self.stop)
 
@@ -226,40 +226,40 @@ class Interpolation(Waveform):
     def __init__(self, x, y, interpolation='linear', **kw):
         super(Interpolation, self).__init__(domain = (x[0], x[-1]), **kw)
         self.xy = (x,y)
-        self.calc = interpolate.interp1d(x, y, kind=interpolation)
-        self._calc_domain = self._domain
+        self.timeFunc = interpolate.interp1d(x, y, kind=interpolation)
+        self._timeFunc_domain = self._domain
 
 class Step(Waveform):
     def __init__(self, width):
         super(Step, self).__init__(domain=(-0.5*width,0.5*width), outside=(0,1))
         self.width = width
         if width == 0:
-            self.calc = lambda x: x>0
+            self.timeFunc = lambda x: x>0
         else:
-            self.calc = lambda x: (erf(5*x/width)+1)/2
-        #self.calc = lambda t: a + (b - a) / (1 + np.exp(-(20*t)/width))
+            self.timeFunc = lambda x: (erf(5*x/width)+1)/2
+        #self.timeFunc = lambda t: a + (b - a) / (1 + np.exp(-(20*t)/width))
 
 class Gaussian(Waveform):
     def __init__(self, width):
         super(Gaussian, self).__init__(domain=(-0.5*width,0.5*width))
         self.width = width
         c = self.width/(4*np.sqrt(2*np.log(2)))
-        self.calc = lambda x: np.exp(-0.5*(x/c)**2)
+        self.timeFunc = lambda x: np.exp(-0.5*(x/c)**2)
 
 class Sin(Waveform):
     def __init__(self, w, phi=0):
         super(Sin, self).__init__(domain=(-np.inf, np.inf))
-        self.calc = lambda t: np.sin(w*t+phi)
+        self.timeFunc = lambda t: np.sin(w*t+phi)
 
 class Cos(Waveform):
     def __init__(self, w, phi=0):
         super(Cos, self).__init__(domain=(-np.inf, np.inf))
-        self.calc = lambda t: np.cos(w*t+phi)
+        self.timeFunc = lambda t: np.cos(w*t+phi)
 
 class Sinc(Waveform):
     def __init__(self, a):
         super(Sinc, self).__init__(domain=(-np.inf, np.inf))
-        self.calc = lambda t: sinc(a*t)
+        self.timeFunc = lambda t: sinc(a*t)
 
 __all__ = ['Waveform', 'DC', 'Interpolation', 'Step', 'Gaussian', 'Sin', 'Cos', 'Sinc']
 
