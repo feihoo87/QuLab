b77201d9709504d5bf4a3214e112286b4f25a416
Li Zhiyuan <32617128+Lagikna@users.noreply.github.com>
2018-11-19 15:13:04 +0800

    modify pxi_awg driver, and basically complete this Driver

--- qulab/drivers/Kesight_PXI/pxi_awg.py
+++ qulab/drivers/Kesight_PXI/pxi_awg.py
@@ -10,14 +10,14 @@ class Driver(BaseDriver):
     support_models = ['M3202A', ]
 
     quants = [
-        QReal('Amplitude', unit='V', ch=0),
+        QReal('Amplitude', value=1, unit='V', ch=0),
         #DC WaveShape
-        QReal('Offset', unit='V', ch=0),
+        QReal('Offset', value=0, unit='V', ch=0),
         #Function Generators(FGs) mode
         QReal('Frequency', unit='Hz', ch=0),
-        QReal('Phase', unit='deg', ch=0),
+        QReal('Phase', value=0, unit='deg', ch=0),
 
-        QOption('WaveShape', ch=0, value='AWG',
+        QOption('WaveShape', ch=0, value='HIZ',
             options = [('HIZ',       -1),
                        ('NoSignal',   0),
                        ('Sin',        1),
@@ -43,6 +43,22 @@ class Driver(BaseDriver):
                                         ('SWtriCycle',   5),
                                         ('External',     2),
                                         ('ExternalCycle',6)]),
+        QOption('triExtSource', value='EXTERN', ch=0,
+                             options = [('EXTERN', 0),
+                                        ('PXI0', 4000),
+                                        ('PXI1', 4001),
+                                        ('PXI2', 4002),
+                                        ('PXI3', 4003),
+                                        ('PXI4', 4004),
+                                        ('PXI5', 4005),
+                                        ('PXI6', 4006),
+                                        ('PXI7', 4007)]),
+        QOption('triggerBehavior', value='RISE', ch=0,
+                             options = [('NONE', 0),
+                                        ('HIGH', 1),
+                                        ('LOW',  2),
+                                        ('RISE', 3),
+                                        ('FALL', 4)]),
         # Defines the delay between the trigger and the waveform launch in tens of ns
         QInteger('startDelay', value='0', unit='ns', ch=0, ),
         # Number of times the waveform is repeated once launched (negative means infinite)
@@ -50,8 +66,11 @@ class Driver(BaseDriver):
         # Waveform prescaler value, to reduce the effective sampling rate
         QInteger('prescaler', value='0', ch=0,),
 
-        QOption('Output', ch=0, value='OFF', options = [('OFF', 0),   ('ON', 1),
-                                                        ('Pause', 2), ('Resume', 3)]),
+        QOption('Output', ch=0, value='Close', options = [('Stop', 0),  ('Run', 1),
+                                                          ('Pause', 2), ('Resume', 3),
+                                                          ('Close', -1)]),
+        QList('WList', value=[]),
+        QList('SList', value=[], ch=0),
     ]
     #CHs : 仪器通道
     CHs=[0,1,2,3]
@@ -82,18 +101,17 @@ class Driver(BaseDriver):
 
     def performClose(self):
         """Perform the close instrument connection operation"""
-        # from labber driver keysight_pxi_awg.py
+        # refer labber driver keysight_pxi_awg.py
         # do not check for error if close was called with an error
         try:
             self.setValue('clockIO','OFF')
             # clear old waveforms and stop awg
-            self.AWG.waveformFlush()
+            self.waveformFlush()
             for ch in range(4):
-                self.AWG.AWGstop(ch)
-                self.AWG.AWGflush(ch)
-                self.AWG.channelWaveShape(ch, -1)
+                self.closeCh(ch)
             # close instrument
             self.AWG.close()
+            # 删除
         except Exception:
             # never return error here
             pass
@@ -101,7 +119,10 @@ class Driver(BaseDriver):
     def closeCh(self,ch=0):
         self.AWG.AWGstop(ch)
         self.AWG.AWGflush(ch)
+        self.config['SList'][ch]=[]
         self.AWG.channelWaveShape(ch, -1)
+        self.config['WaveShape'][ch]['value']='HIZ'
+        self.config['Output'][ch]['value']='Close'
 
     def performSetValue(self, quant, value, ch=0, **kw):
         _cfg={}
@@ -130,14 +151,19 @@ class Driver(BaseDriver):
             self.AWG.triggerIOconfigV5(*options[value])
             ch=0
         elif quant.name == 'Output':
-            if value=='OFF':
+            if value=='Stop':
                 self.AWG.AWGstop(ch)
-            elif value=='ON':
+            elif value=='Start':
                 self.AWG.AWGstart(ch)
             elif value=='Pause':
                 self.AWG.AWGpause(ch)
             elif value=='Resume':
                 self.AWG.AWGresume(ch)
+            elif value=='Close':
+                self.closeCh(ch)
+        elif quant.name == 'clockSyncFrequency':
+            print("clockSyncFrequency can't be set")
+            return
         _cfg['value']=value
         self.config[quant.name][ch].update(_cfg)
 
@@ -152,11 +178,15 @@ class Driver(BaseDriver):
             value=self.AWG.clockGetSyncFrequency()
             ch=0
             _cfg['value']=value
+        elif quant.name in ['clockIO','triggerIO']:
+            ch=0
         self.config[quant.name][ch].update(_cfg)
         return self.config[quant.name][ch]['value']
 
     def phaseReset(self,ch=0):
         self.AWG.channelPhaseReset(ch)
+        _cfg={'value':0}
+        self.config['Phase'][ch].update(_cfg)
 
     def clockResetPhase(self):
         # self.AWG.clockResetPhase(triggerBehavior, triggerSource, skew = 0.0)
@@ -181,25 +211,56 @@ class Driver(BaseDriver):
             return wave
 
     def waveformLoad(self, waveform, num, paddingMode = 0):
-        '''num: waveform_num, 在板上内存的编号'''
-        self.AWG.waveformLoad(waveform, num, paddingMode)
+        '''num: waveform_num, 在板上内存的波形编号'''
+        if num in self.config['WList'][0]:
+            self.AWG.waveformReLoad(waveform, num, paddingMode)
+        else:
+            # This function replaces a waveform located in the module onboard RAM.
+            # The size of the newwaveform must be smaller than or equal to the existing waveform.
+            self.AWG.waveformLoad(waveform, num, paddingMode)
+            self.config['WList'][0].append(num)
 
-    def waveformReLoad(self, waveform, num, paddingMode = 0):
-        '''This function replaces a waveform located in the module onboard RAM.
-        The size of the newwaveform must be smaller than or equal to the existing waveform.'''
-        self.AWG.waveformReLoad(waveform, num, paddingMode)
+    # def waveformReLoad(self, waveform, num, paddingMode = 0):
+    #     '''This function replaces a waveform located in the module onboard RAM.
+    #     The size of the newwaveform must be smaller than or equal to the existing waveform.'''
+    #     self.AWG.waveformReLoad(waveform, num, paddingMode)
 
     def waveformFlush(self):
         '''This function deletes all the waveforms from the module onboard RAM
         and flushes all the AWG queues'''
         self.AWG.waveformFlush()
+        self.config['WList'][0]=[]
 
     def AWGqueueWaveform(self, ch=0, waveform_num=0):
-        triggerMode = self.getCmdOption('triggerMode')
-        startDelay = self.getValue('startDelay')
-        cycles = self.getValue('cycles')
-        prescaler = self.getValue('prescaler')
+        self.setValue('WaveShape', 'AWG', ch=ch)
+        Amplitude=self.getValue('Amplitude',ch=ch)
+        self.setValue('Amplitude', Amplitude, ch=ch)
+
+        triggerModeIndex = self.getValue('triggerMode',ch=ch)
+        triggerModeOptions=self.quantities['triggerMode'].options
+        triggerMode = dict(triggerModeOptions)[triggerModeIndex]
+
+        if triggerModeIndex in ['External','ExternalCycle']:
+
+            triExtSourceIndex = self.getValue('triExtSource',ch=ch)
+            triExtSourceOptions=self.quantities['triExtSource'].options
+            triExtSource = dict(triExtSourceOptions)[triExtSourceIndex]
+            if triExtSourceIndex in ['EXTERN']:
+                # 若未设置过，则从config读取默认配置；若已设置，则结果不变
+                triggerIO = self.getValue('triggerIO')
+                self.setValue('triggerIO', triggerIO)
+
+            triggerBehaviorIndex = self.getValue('triggerBehavior',ch=ch)
+            triggerBehaviorOptions=self.quantities['triggerBehavior'].options
+            triggerBehavior = dict(triggerBehaviorOptions)[triggerBehaviorIndex]
+
+            self.AWG.AWGtriggerExternalConfig(ch, triExtSource, triggerBehavior)
+
+        startDelay = self.getValue('startDelay',ch=ch)
+        cycles = self.getValue('cycles',ch=ch)
+        prescaler = self.getValue('prescaler',ch=ch)
         self.AWG.AWGqueueWaveform(ch, waveform_num, triggerMode, startDelay, cycles, prescaler)
+        self.config['SList'][ch].append(waveform_num)
 
 
 
