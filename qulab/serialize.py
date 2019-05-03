f5497159f7705e829ce725bbc6cd3c53790a974c
feihoo87 <xfh1987@gmail.com>
2019-05-03 13:26:36 +0800

    feat(serialize): add `register`
    Serializing and deserializing user-defined types supported.

--- qulab/serialize.py
+++ qulab/serialize.py
@@ -1,13 +1,32 @@
 import pickle
 import struct
-from typing import Any
+from typing import Any, Callable, TypeVar
 
+import numpy as np
 import umsgpack
+
+__index = 0
+__pack_handlers = {}
+__unpack_handlers = {}
+
+T = TypeVar('T')
+
+
+def register(cls: type, encode: Callable[[T], bytes],
+             decode: Callable[[bytes], T]) -> None:
+    global __index
+    __index += 1
+    t = __index
+    __pack_handlers[cls] = lambda obj: umsgpack.Ext(t, encode(obj))
+    __unpack_handlers[t] = lambda ext: decode(ext.data)
 
 
 def pack(obj: Any) -> bytes:
-    return umsgpack.packb(obj)
+    return umsgpack.packb(obj, ext_handlers=__pack_handlers)
 
 
 def unpack(buff: bytes) -> Any:
-    return umsgpack.unpackb(buff)
+    return umsgpack.unpackb(buff, ext_handlers=__unpack_handlers)
+
+
+register(np.ndarray, lambda x: x.tobytes(), np.frombuffer)
--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 from qulab.serialize import *
 
@@ -6,3 +8,21 @@ def test_serialize():
     buff = pack((123, 456))
     assert isinstance(buff, bytes)
     assert unpack(buff) == [123, 456]
+    x = np.linspace(0, 1, 101)
+    buff = pack(x)
+    assert isinstance(buff, bytes)
+    assert np.all(x == unpack(buff))
+
+
+class A:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+def test_register():
+    register(A, pickle.dumps, pickle.loads)
+    assert A(3, 5) == unpack(pack(A(3, 5)))
