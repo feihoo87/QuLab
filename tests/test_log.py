5a98672ddc6273f8c2deff32966d387e4fddd06e
feihoo87 <xfh1987@gmail.com>
2019-05-05 02:12:36 +0800

    test(log): change test.

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,11 +1,10 @@
 import logging
+import time
 
 import pytest
 import zmq
 from qulab.log import *
 from qulab.serialize import unpack
-
-ctx = zmq.Context()
 
 
 def test_level():
@@ -23,9 +22,12 @@ def test_BaseHandler():
     record2 = logging.makeLogRecord(unpack(bmsg))
     assert record2.name == hdlr.host + '.' + record.name
     assert record2.msg == record.msg
+    with pytest.raises(NotImplementedError):
+        hdlr.send_bytes(b'')
 
 
 def test_logger():
+    ctx = zmq.Context()
     interface = "tcp://127.0.0.1"
     with ctx.socket(zmq.PUB) as pub, ctx.socket(zmq.SUB) as sub:
         sub.setsockopt(zmq.LINGER, 0)
@@ -35,6 +37,7 @@ def test_logger():
         sub.connect('%s:%s' % (interface, port))
 
         sub.subscribe(b'')
+        time.sleep(0.1)
 
         logger = logging.getLogger('qulabtest')
         logger.setLevel(logging.DEBUG)
@@ -43,7 +46,7 @@ def test_logger():
         logger.addHandler(handler)
 
         logger.debug('hello')
-        if sub.poll(1000):
+        if sub.poll(100):
             btopic, bmsg = sub.recv_multipart()
             record = logging.makeLogRecord(unpack(bmsg))
             assert record.msg == 'hello'
