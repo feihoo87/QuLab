9cb289c4ab1d5d8f60c5ef3c6e3eaf2f4e4de8ac
feihoo87 <xfh1987@gmail.com>
2019-05-16 14:40:48 +0800

    tests(loader): test driver loader

---
+++ tests/test_loader.py
@@ -0,0 +1,8 @@
+from qulab.loader import loadDriver
+
+def test_loadDriver():
+    Driver = loadDriver('Tek_AWG')
+    assert hasattr(Driver, 'create_waveform')
+    Driver = loadDriver('PSG')
+    assert Driver.support_models == ['E8257D', 'SMF100A', 'SMB100A', 'SGS100A']
+    
