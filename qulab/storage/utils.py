ee2b2290b39c95b1364d9922e622b97df9349d31
feihoo87 <xfh1987@gmail.com>
2019-05-20 13:56:20 +0800

    feat(storage): new function `save`

---
+++ qulab/storage/utils.py
@@ -0,0 +1,18 @@
+import time
+from pathlib import Path
+
+import numpy as np
+from qulab import config
+
+
+def save(title, *args, base_path=None, **kw):
+    if base_path is None:
+        base_path = Path(config.get('data_path', Path.cwd()))
+    else:
+        base_path = Path(base_path)
+
+    path = base_path / time.strftime('%Y') / time.strftime('%m%d')
+    fname = f"{title}_{time.strftime('%Y%m%d%H%M%S')}.npz"
+    path.mkdir(parents=True, exist_ok=True)
+    np.savez_compressed(path / fname, *args, **kw)
+    return path / fname
