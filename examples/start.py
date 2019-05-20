fa74facb92b4cf76e9e630d186b5259c1f5baf4e
feihoo87 <xfh1987@gmail.com>
2019-05-20 21:03:16 +0800

    fix(example): fix instrument name error

--- examples/start.py
+++ examples/start.py
@@ -8,7 +8,7 @@ scripts = [
     'instrument.py --name=AWG --model=AWG5014C --driver=TekAWG --address=TCPIP::192.168.2.106',
     'instrument.py --name=EX --model=E8257D --driver=PSG --address=TCPIP0::192.168.2.102',
     'instrument.py --name=LO --model=D8257D --driver=PSG --address=TCPIP0::192.168.2.121',
-    'instrument.py --name=LO --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.2.121',
+    'instrument.py --name=NA --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.2.121',
     'jupyter-notebook.py --notebook-dir=C:/WPy64-3720/notebooks',
 ]
 
