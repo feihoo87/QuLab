46e6f128e5e760ca0c95f20a5984ce171c3de828
feihoo87 <xfh1987@gmail.com>
2019-05-23 23:01:17 +0800

    fix(examples): correct ip address of network analyzer

--- examples/start.py
+++ examples/start.py
@@ -9,7 +9,7 @@ scripts = [
     'instrument.py --name=AWG --model=AWG5014C --driver=TekAWG --address=TCPIP::192.168.2.106',
     'instrument.py --name=EX --model=E8257D --driver=PSG --address=TCPIP0::192.168.2.102',
     'instrument.py --name=LO --model=D8257D --driver=PSG --address=TCPIP0::192.168.2.121',
-    'instrument.py --name=NA --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.2.121',
+    'instrument.py --name=NA --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.2.114',
     'jupyter-notebook.py --notebook-dir=D:/notebooks',
 ]
 
