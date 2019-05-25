53de9b251acd1d78991b5031a81a3c02e39df3de
feihoo87 <xfh1987@gmail.com>
2019-05-25 22:12:26 +0800

    test(job): test job and db storage

---
+++ tests/test_job.py
@@ -0,0 +1,44 @@
+import asyncio
+
+import numpy as np
+import pytest
+
+from qulab.job import Job
+from qulab.storage.schema import Record
+import inspect
+
+
+@pytest.fixture
+def mongo_db():
+    from qulab.storage.connect import get_connection
+    conn = get_connection()
+    conn.drop_database('lab')
+    yield
+    conn.drop_database('lab')
+
+
+async def mw(flist):
+    for i in flist:
+        await asyncio.sleep(0)
+        yield i, 0
+
+
+async def spec(y=0):
+    for i in range(10):
+        args = (np.linspace(0, 1, 11), )
+        a = Job(mw, args, max=11)
+        x, y = await a.done()
+        yield i, x, y
+
+
+@pytest.mark.asyncio
+async def test_job(mongo_db):
+    job = Job(spec, max=10, tags=['x', 'y'], comment='hello, world')
+    await job.done()
+    record = Record.objects()[0]
+    assert record.title == 'spec'
+    assert record.work.text == inspect.getsource(spec)
+    i, x, y = record.data
+    assert np.all(i == np.arange(10))
+    assert np.all(x[0] == np.linspace(0, 1, 11))
+    assert np.all(y == 0)
