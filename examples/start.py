ca1fb2e482290bcc81d6a04c5d668a70a38560a3
feihoo87 <xfh1987@gmail.com>
2019-05-29 20:54:46 +0800

    feat(examples): update exanples

--- examples/start.py
+++ examples/start.py
@@ -1,20 +1,17 @@
 import os
 import sys
 import time
-from pathlib import Path
 
 scripts = [
-    'dht.py',
-    'instrument.py --name=ATS --model=ATS9870 --driver=AlazarTechDigitizer --address=ATS9870::SYSTEM1::1::INSTR --no-visa',
-    'instrument.py --name=AWG --model=AWG5014C --driver=TekAWG --address=TCPIP::192.168.2.106',
-    'instrument.py --name=EX --model=E8257D --driver=PSG --address=TCPIP0::192.168.2.102',
-    'instrument.py --name=LO --model=D8257D --driver=PSG --address=TCPIP0::192.168.2.121',
-    'instrument.py --name=NA --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.2.114',
-    'jupyter-notebook.py --notebook-dir=D:/notebooks',
+    'qulab.cli.dht',
+    'qulab.cli.instrument --name=ATS --model=ATS9870 --driver=AlazarTechDigitizer --address=ATS9870::SYSTEM1::1::INSTR --no-visa',
+    'qulab.cli.instrument --name=AWG --model=AWG5014C --driver=TekAWG --address=TCPIP::192.168.1.106',
+    'qulab.cli.instrument --name=LO --model=E8257D --driver=PSG --address=TCPIP0::192.168.1.102',
+    'qulab.cli.instrument --name=EX --model=D8257D --driver=PSG --address=TCPIP0::192.168.1.121',
+    'qulab.cli.instrument --name=NA --model=N5232A --driver=NetworkAnalyzer --address=TCPIP0::192.168.1.114',
+    'qulab.cli.notebook --notebook-dir=D:/notebooks',
 ]
 
-p = Path(__file__).parent.resolve()
-
 for cmd in scripts:
-    os.system('cmd /c start %s %s\\%s' % (sys.executable, p, cmd))
+    os.system('cmd /c start %s %s\\%s' % (sys.executable, cmd))
     time.sleep(1)
