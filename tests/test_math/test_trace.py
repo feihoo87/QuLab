6b11662c42baa1c993b608c7e5e41a2d5a5ced4b
feihoo87 <xfh1987@gmail.com>
2019-06-12 12:18:59 +0800

    test(trace): fix compare error

--- tests/test_math/test_trace.py
+++ tests/test_math/test_trace.py
@@ -9,15 +9,15 @@ from qulab.math.trace import trace
 
 def test_trace():
     N = 5
-    mat_lst = [np.random.randint(500, size=(2, 2)) for i in range(N)]
+    mat_lst = [np.random.randn(2, 2) for i in range(N)]
     A = reduce(np.kron, mat_lst)
-    
+
     for sub_mask in product([0, 1], repeat=N):
         res1 = trace(A, sub_mask)
         res2 = reduce(np.kron, compress(
             mat_lst, 1 - np.array(sub_mask)), 1) * reduce(
                 operator.mul, map(np.trace, compress(mat_lst, sub_mask)), 1)
-        assert np.all(res1 == res2)
+        assert np.all(np.abs(res1 - res2) < 1e-12)
 
     assert np.all(trace(A, '01100') == trace(A, [0, 1, 1, 0, 0]))
     assert np.all(trace(A, '01100') == trace(A, 12))
