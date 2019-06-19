b66e3ad250f31a54058380dcc9d3b620d0a05d7c
feihoo87 <xfh1987@gmail.com>
2019-06-19 17:28:17 +0800

    feat(math): add optimize

--- qulab/math/__init__.py
+++ qulab/math/__init__.py
@@ -2,3 +2,4 @@ from qulab.math.func import (FWHM_of_normal_distribution,
                              Std_of_norm_from_FWHM, get_probility,
                              get_threshold_visibility, get_unit_prefix,
                              kurtosis, skew, threshold, vonNeumannEntropy)
+from qulab.math.opt import optimize
---
+++ tests/test_math/test_opt.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from qulab.math import optimize
+
+
+def target(x, y, z):
+    return (x - 1)**2 + (y + np.e)**2 + (z - np.pi)**2
+
+
+def test_optimize():
+    ret = optimize(target,
+                   start=[0, 0, 0],
+                   senstive=[0.1, 0.1, 0.1],
+                   dec=[2, 5, 7])
+    assert np.all(np.abs(ret['x'] - np.array([1., -np.e, np.pi])) < 1e-5)
