d04d0913c1e40124b9285b6d3aeaa0ec48e1ccd7
feihoo87 <xfh1987@gmail.com>
2019-07-09 00:10:15 +0800

    feat(math): optimize support coroutine functions

--- tests/test_math/test_opt.py
+++ tests/test_math/test_opt.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import numpy as np
 
 from qulab.math import optimize
@@ -7,8 +9,21 @@ def target(x, y, z):
     return (x - 1)**2 + (y + np.e)**2 + (z - np.pi)**2
 
 
+async def async_target(x, y, z):
+    await asyncio.sleep(0.001)
+    return target(x, y, z)
+
+
 def test_optimize():
     ret = optimize(target,
+                   start=[0, 0, 0],
+                   senstive=[0.1, 0.1, 0.1],
+                   dec=[2, 5, 7])
+    assert np.all(np.abs(ret['x'] - np.array([1., -np.e, np.pi])) < 1e-5)
+
+
+def test_optimize_async():
+    ret = optimize(async_target,
                    start=[0, 0, 0],
                    senstive=[0.1, 0.1, 0.1],
                    dec=[2, 5, 7])
