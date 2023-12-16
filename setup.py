1fd13275441f7620c8de0031bfee83258b16123c
feihoo87 <xfh1987@gmail.com>
2023-12-16 16:20:21 +0800

    add setup

--- setup.py
+++ setup.py
@@ -1,86 +1,31 @@
-import platform
-from codecs import open
-from os import getcwd, getenv, listdir, path
+import os
 
-from setuptools import find_packages, setup
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def module_name(dirpath, filename):
+    l = [os.path.splitext(filename)[0]]
+    head = dirpath
+    while True:
+        head, tail = os.path.split(head)
+        l.append(tail)
+        if not head:
+            break
+    return '.'.join(reversed(l))
 
-# This reads the __version__ variable from lab/_version.py
-exec(open('qulab/_version.py').read())
 
-requirements = [
-    'aioredis>=1.2.0',
-    'blinker>=1.4',
-    'cryptography>=2.6.1',
-    'ipython>=7.4.0',
-    'ipywidgets>=7.4.2',
-    'mongoengine>=0.17.0',
-    'nest-asyncio>=1.0.0',
-    'numpy>=1.13.3',
-    'scipy>=1.0.0',
-    'portalocker>=1.4.0',
-    'PyVISA>=1.9.1',
-    'PyVISA-py>=0.3.1',
-    'PyYAML>=5.1',
-    'pyzmq>=18.0.1',
-    'redis>=3.2.1',
-    'contextvars;python_version<"3.7"',
-    'dataclasses;python_version<"3.7"',
-]
+def get_extensions():
+    extensions = []
+    for dirpath, dirnames, filenames in os.walk('cycles'):
+        for filename in filenames:
+            if filename.endswith('.pyx'):
+                extensions.append(
+                    Extension(module_name(dirpath, filename),
+                              [os.path.join(dirpath, filename)]))
 
-if platform.system() == 'Windows':
-    requirements.extend([
-        'pywin32>=224'
-    ])
+    return extensions
 
-setup(
-    name="QuLab",
-    version=__version__,
-    author="feihoo87",
-    author_email="feihoo87@gmail.com",
-    url="https://github.com/feihoo87/QuLab",
-    license = "MIT",
-    keywords="experiment laboratory",
-    description="contral instruments and manage data",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages = find_packages(),
-    include_package_data = True,
-    #data_files=[('QuLab/Drivers', driverFiles)],
-    install_requires=requirements,
-    extras_require={
-        'test': [
-            'pytest>=4.4.0',
-            'pytest-asyncio>=0.10.0',
-            'pyvisa-sim>=0.3',
-        ],
-        'docs': [
-            'Sphinx',
-            'sphinxcontrib-napoleon',
-            'sphinxcontrib-zopeext',
-        ],
-    },
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
-        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/feihoo87/QuLab/issues',
-        'Source': 'https://github.com/feihoo87/QuLab/',
-    },
-)
+
+setup(packages=find_packages(exclude=['tests']),
+      ext_modules=cythonize(get_extensions(), build_dir=f"build"))
---
+++ src/qulab.h
@@ -0,0 +1,5 @@
+#ifndef __QULAB_H__
+#define __QULAB_H__
+
+
+#endif // __QULAB_H__
