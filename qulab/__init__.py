429dd233e8cf6ae5f68b9def2d01cd58ef23e0ee
feihoo87 <xfh1987@gmail.com>
2024-06-05 22:41:32 +0800

    update

--- qulab/__init__.py
+++ qulab/__init__.py
@@ -1,3 +1,4 @@
-from .scan import Scan, get_record, load_record, lookup, lookup_list
+from .scan import (Scan, get_ipython_history, get_record, load_record, lookup,
+                   lookup_list)
 from .version import __version__
 from .visualization import autoplot
