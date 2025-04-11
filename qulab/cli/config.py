98a6160bc7c4d3e92613040c025e7c4f07923f0c
feihoo87 <xfh1987@gmail.com>
2025-04-11 11:14:07 +0800

    update config

--- qulab/cli/config.py
+++ qulab/cli/config.py
@@ -11,7 +11,10 @@ CONFIG_PATH = os.path.expanduser("~/.qulab.ini")
 ENV_PREFIX = "QULAB_"
 
 
-def _get_config_value(option_name, type_cast=str, command_name=None):
+def _get_config_value(option_name,
+                      type_cast=str,
+                      command_name=None,
+                      default=None):
     """支持命令专属配置的优先级获取"""
     # 构造环境变量名
     if command_name:
@@ -47,13 +50,51 @@ def _get_config_value(option_name, type_cast=str, command_name=None):
                 return os.path.expanduser(config_value)
             return type_cast(config_value)
 
-    return None  # 交给 Click 处理默认值
+    return default  # 交给 Click 处理默认值
 
 
-def get_config_value(option_name, type_cast=str, command_name=None):
-    value = _get_config_value(option_name, type_cast, command_name)
+def get_config_value(option_name,
+                     type_cast=str,
+                     command_name=None,
+                     default=None):
+    """
+    获取配置值，支持命令专属配置的优先级获取
+
+    优先级：
+    1. 命令行参数
+    2. 配置文件专属配置
+    3. 配置文件公共配置
+    4. 环境变量专属配置
+    5. 环境变量公共配置
+    6. 默认值
+    7. Click 处理默认值
+    8. None
+
+    Parameters
+    ----------
+    option_name : str
+        配置项名称
+    type_cast : type
+        转换类型
+    command_name : str
+        命令名称
+        如果为 None，则不使用命令专属配置
+    default : any
+        默认值
+        如果为 None，则不使用默认值
+    
+    Returns
+    -------
+    any
+        配置值
+        如果没有找到配置值，则返回 None
+    """
+    value = _get_config_value(option_name,
+                              type_cast,
+                              command_name,
+                              default=default)
     if value is None and command_name is not None:
-        return _get_config_value(option_name, type_cast)
+        return _get_config_value(option_name, type_cast, default=default)
     return value
 
 
