5038d464dae3300419120a8255c4532ec3f6bbe1
feihoo87 <xfh1987@gmail.com>
2025-04-11 09:59:11 +0800

    fix reproduce

--- qulab/executor/load.py
+++ qulab/executor/load.py
@@ -1,7 +1,7 @@
-import graphlib
+import atexit
 import inspect
 import pickle
-import sys
+import tempfile
 import warnings
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
@@ -333,17 +333,18 @@ def load_workflow_from_file(file_name: str,
 def load_workflow_from_source_code(workflow_id: str,
                                    source_code: str,
                                    package='workflows') -> WorkflowType:
-    path = Path(workflow_id)
-    module_name = f"{package}.{'.'.join([*path.parts[:-1], path.stem])}"
+    workflow_path = Path(workflow_id)
+    module_name = f"{package}.{'.'.join([*workflow_path.parts[:-1], workflow_path.stem])}"
 
-    # 创建一个新的模块对象
-    module = ModuleType(module_name)
-    # 将模块注册到sys.modules中
-    sys.modules[module_name] = module
-    # 将源代码编译成字节码
-    code = compile(source_code, '<string>', 'exec')
-    # 在模块的命名空间中执行字节码
-    exec(code, module.__dict__)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.py').name
+    with open(temp_file, 'w') as f:
+        f.write(source_code)
+
+    atexit.register(lambda f=temp_file: Path(f).unlink(missing_ok=True))
+
+    spec = spec_from_file_location(module_name, temp_file)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
 
     module.__source__ = source_code
     module.__mtime__ = 0
