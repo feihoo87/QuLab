9837f2b2217b6e28f16c13f2484ca7417d8e1bf2
feihoo87 <xfh1987@gmail.com>
2025-05-07 16:59:18 +0800

    update namespace

--- qulab/__init__.py
+++ qulab/__init__.py
@@ -1,3 +1,27 @@
+from qlisp import (CR, CX, CZ, SWAP, A, B, BellPhiM, BellPhiP, BellPsiM,
+                   BellPsiP, H, S, Sdag, SQiSWAP, T, Tdag, U, Unitary2Angles,
+                   applySeq, draw, fSim, iSWAP, kak_decomposition, kak_vector,
+                   make_immutable, measure, phiminus, phiplus, psiminus,
+                   psiplus, regesterGateMatrix, rfUnitary, seq2mat, sigmaI,
+                   sigmaM, sigmaP, sigmaX, sigmaY, sigmaZ,
+                   synchronize_global_phase)
+from qlispc import (COMMAND, FREE, NOTSET, PUSH, READ, SYNC, TRIG, WRITE,
+                    compile, get_arch, libraries, mapping_qubits,
+                    register_arch)
+from qlispc.kernel_utils import qcompile
+from wath import (Interval, Primes, Transmon, Z2probs, complex_amp_to_real,
+                  effective_temperature, exception, find_axis_of_symmetry,
+                  find_center_of_symmetry, find_cross_point, fit_circle,
+                  fit_cosine, fit_k, fit_max, fit_peaks, fit_pole, getFTMatrix,
+                  graph, inv_poly, lin_fit, point_in_ellipse, point_in_polygon,
+                  point_on_ellipse, point_on_segment, poly_fit, probs2Z,
+                  relative_delay_to_absolute, thermal_excitation, viterbi_hmm)
+from waveforms import (D, chirp, const, cos, cosh, coshPulse, cosPulse, cut,
+                       drag, drag_sin, drag_sinx, exp, function, gaussian,
+                       general_cosine, hanning, interp, mixing, one, poly,
+                       registerBaseFunc, registerDerivative, samplingPoints,
+                       sign, sin, sinc, sinh, square, step, t, wave_eval, zero)
+
 from .executor.analyze import manual_analysis
 from .executor.registry import Registry
 from .executor.storage import find_report
